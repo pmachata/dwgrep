05888fb1bf5aaa7b9f35fe4a87754e1a8428c370
Petr Machata <pmachata@redhat.com>
2014-03-25 23:25:35 +0100

    Add a separate executor hierarchy to coroutine prototype
    - This allows to carry pre-compiled queries around, and re-execute
      them sereval times on different queries.  State is kept in the
      exec_node/que_node hierarchy, expr hierarchy just knows how to
      instantiate this when needed.
    
    - This also solves the problem of obtaining "query" argument in
      call_operate: query can be passed to ctor of those nodes that need
      access to it.

--- pipe.cc
+++ pipe.cc
@@ -11,6 +11,10 @@
 
 #include "make_unique.hh"
 
+class expr;
+class que_node;
+class exec_node;
+
 namespace
 {
   inline void
@@ -27,8 +31,8 @@ struct valfile
   typedef std::unique_ptr <valfile> ptr;
 };
 
-class op;
-class leaf_op;
+class expr;
+class exec_node;
 
 class que
 {
@@ -37,9 +41,6 @@ class que
   unsigned m_head;
   unsigned m_tail;
 
-  leaf_op &m_left;
-  leaf_op &m_right;
-
   static unsigned
   wrap (unsigned i)
   {
@@ -47,7 +48,10 @@ class que
   }
 
 public:
-  que (leaf_op &left, leaf_op &right);
+  que ()
+    : m_head (0)
+    , m_tail (0)
+  {}
 
   bool
   empty () const
@@ -77,47 +81,130 @@ public:
     m_tail = wrap (m_tail + 1);
     return std::move (m_que[idx]);
   }
-
-  void switch_right (ucontext &cur);
-  void switch_left (ucontext &cur);
 };
 
-class in_que
+struct query
 {
-  que &m_que;
+  typedef std::shared_ptr <query> ptr;
+};
+
+class expr
+{
+public:
+  virtual ~expr () {}
+
+  virtual std::pair <std::shared_ptr <exec_node>, std::shared_ptr <exec_node> >
+  build_exec (query::ptr q) = 0;
+};
+
+class exec_node
+{
+  static size_t const stack_size = 4 * 4096;
+
+  std::shared_ptr <que_node> m_in_que;
+  std::shared_ptr <que_node> m_out_que;
+
+  std::unique_ptr <ucontext> m_uc;
+  int m_vg_stack_id;
+
+  static void
+  call_operate (exec_node *self)
+  {
+    self->operate ();
+  }
+
+protected:
+  valfile::ptr next ();
+  void push (valfile::ptr vf);
 
 public:
-  in_que (que &q)
-    : m_que (q)
+  exec_node ()
+  {
+    m_uc = std::make_unique <ucontext> ();
+    if (getcontext (&*m_uc) == -1)
+      throw_system ();
+
+    void *stack = mmap (0, stack_size, PROT_READ|PROT_WRITE,
+			MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+
+    if (stack == MAP_FAILED)
+      throw_system ();
+
+    m_vg_stack_id = VALGRIND_STACK_REGISTER
+      (stack, static_cast <char *> (stack) + stack_size);
+    m_uc->uc_stack.ss_sp = stack;
+    m_uc->uc_stack.ss_size = stack_size;
+    m_uc->uc_stack.ss_flags = 0;
+
+    makecontext (&*m_uc, (void (*)()) call_operate, 1, this);
+  }
+
+  virtual ~exec_node ()
+  {
+    VALGRIND_STACK_DEREGISTER (m_vg_stack_id);
+    munmap (m_uc->uc_stack.ss_sp, stack_size);
+  }
+
+  virtual void operate () = 0;
+  virtual std::string name () const = 0;
+
+  void
+  assign_in (std::shared_ptr <que_node> q)
+  {
+    m_in_que = q;
+  }
+
+  void
+  assign_out (std::shared_ptr <que_node> q)
+  {
+    m_out_que = q;
+  }
+
+  void
+  activate (ucontext &other)
+  {
+    //std::cout << "activate " << name () << std::endl;
+    swapcontext (&other, &*m_uc);
+  }
+
+protected:
+  void
+  quit (ucontext &main)
+  {
+    assert (m_uc != nullptr);
+    swapcontext (&*m_uc, &main);
+  }
+};
+
+class que_node
+{
+  que m_que;
+  std::shared_ptr <exec_node> m_left;
+  std::shared_ptr <exec_node> m_right;
+
+public:
+  que_node (std::shared_ptr <exec_node> left, std::shared_ptr <exec_node> right)
+    : m_left ((assert (left != nullptr), left))
+    , m_right ((assert (right != nullptr), right))
   {}
+
+  void
+  switch_right (ucontext &cur)
+  {
+    m_right->activate (cur);
+  }
+
+  void
+  switch_left (ucontext &cur)
+  {
+    m_left->activate (cur);
+  }
 
   valfile::ptr
   pop_front ()
   {
     return m_que.pop_front ();
   }
-
-  bool
-  empty () const
-  {
-    return m_que.empty ();
-  }
-
-  void
-  switch_left (ucontext &cur)
-  {
-    m_que.switch_left (cur);
-  }
-};
-
-class out_que
-{
-  que &m_que;
-
-public:
-  out_que (que &q)
-    : m_que (q)
-  {}
 
   void
   push_back (valfile::ptr vf)
@@ -136,327 +223,259 @@ public:
   {
     return m_que.full ();
   }
-
-  void
-  switch_right (ucontext &cur)
-  {
-    m_que.switch_right (cur);
-  }
 };
 
-struct query
+valfile::ptr
+exec_node::next ()
 {
-};
+  while (m_in_que->empty () && ! m_out_que->empty ())
+    m_out_que->switch_right (*m_uc);
 
-class op
-{
-public:
-  virtual ~op () {}
+  if (m_in_que->empty ())
+    m_in_que->switch_left (*m_uc);
 
-  virtual leaf_op &leftmost () = 0;
-  virtual leaf_op &rightmost () = 0;
-};
+  if (m_in_que->empty ())
+    // If the in que is still empty, we are done.
+    return nullptr;
 
-class leaf_op
-  : public op
-{
-  static size_t const stack_size = 4 * 4096;
-  std::unique_ptr <in_que> m_in_que;
-  std::unique_ptr <out_que> m_out_que;
-  std::unique_ptr <ucontext> m_uc;
-  int m_vg_stack_id;
-
-  static void
-  call_operate (leaf_op *self)
-  {
-    std::cout << "passing NULL query object!!\n";
-    self->operate (* (query *) nullptr);
-  }
-
-protected:
-  valfile::ptr
-  next ()
-  {
-    while (m_in_que->empty () && ! m_out_que->empty ())
-      m_out_que->switch_right (*m_uc);
-
-    if (m_in_que->empty ())
-      m_in_que->switch_left (*m_uc);
-
-    if (m_in_que->empty ())
-      {
-	std::cout << "in que still empty.  Done?\n";
-	return nullptr;
-      }
-
-    //std::cout << name () << " pops\n";
-    return m_in_que->pop_front ();
-  }
-
-  void
-  push (valfile::ptr vf)
-  {
-    while (m_out_que->full ())
-      m_out_que->switch_right (*m_uc);
-
-    //std::cout << name () << " pushes\n";
-    m_out_que->push_back (std::move (vf));
-  }
-
-public:
-  leaf_op ()
-  {
-  }
-
-  ~leaf_op ()
-  {
-    if (m_uc != nullptr)
-      {
-	VALGRIND_STACK_DEREGISTER (m_vg_stack_id);
-	munmap (m_uc->uc_stack.ss_sp, stack_size);
-      }
-  }
-
-  virtual void operate (query const &q) = 0;
-  virtual std::string name () const = 0;
-
-  leaf_op &leftmost () override { return *this; }
-  leaf_op &rightmost () override { return *this; }
-
-  void
-  assign_in (que &q)
-  {
-    m_in_que = std::make_unique <in_que> (q);
-  }
-
-  void
-  assign_out (que &q)
-  {
-    m_out_que = std::make_unique <out_que> (q);
-  }
-
-  void
-  activate (ucontext &other)
-  {
-    if (m_uc == nullptr)
-      {
-	m_uc = std::make_unique <ucontext> ();
-	if (getcontext (&*m_uc) == -1)
-	  throw_system ();
-
-	void *stack = mmap (0, stack_size, PROT_READ|PROT_WRITE,
-			    MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
-
-	if (stack == MAP_FAILED)
-	  throw_system ();
-
-	m_vg_stack_id = VALGRIND_STACK_REGISTER
-	  (stack, static_cast <char *> (stack) + stack_size);
-	m_uc->uc_stack.ss_sp = stack;
-	m_uc->uc_stack.ss_size = stack_size;
-	m_uc->uc_stack.ss_flags = 0;
-
-	makecontext (&*m_uc, (void (*)()) call_operate, 1, this);
-      }
-
-    //std::cout << "activate " << name () << std::endl;
-    swapcontext (&other, &*m_uc);
-  }
-
-protected:
-  void
-  quit (ucontext &main)
-  {
-    assert (m_uc != nullptr);
-    swapcontext (&*m_uc, &main);
-  }
-};
-
-que::que (leaf_op &left, leaf_op &right)
-  : m_head (0)
-  , m_tail (0)
-  , m_left (left)
-  , m_right (right)
-{
-  m_left.assign_out (*this);
-  m_right.assign_in (*this);
+  //std::cout << name () << " pops\n";
+  return m_in_que->pop_front ();
 }
 
 void
-que::switch_right (ucontext &cur)
+exec_node::push (valfile::ptr vf)
 {
-  //std::cout << "switching right" << std::endl;
-  m_right.activate (cur);
+  while (m_out_que->full ())
+    m_out_que->switch_right (*m_uc);
+
+  //std::cout << name () << " pushes\n";
+  m_out_que->push_back (std::move (vf));
 }
 
-void
-que::switch_left (ucontext &cur)
+class expr_cat
+  : public expr
 {
-  //std::cout << "switching left" << std::endl;
-  m_left.activate (cur);
-}
-
-class op_cat
-  : public op
-{
-  std::unique_ptr <op> m_left;
-  std::unique_ptr <op> m_right;
-  que m_que;
+  std::unique_ptr <expr> m_left;
+  std::unique_ptr <expr> m_right;
 
 public:
-  op_cat (std::unique_ptr <op> left, std::unique_ptr <op> right)
+  expr_cat (std::unique_ptr <expr> left, std::unique_ptr <expr> right)
     : m_left (std::move (left))
     , m_right (std::move (right))
-    , m_que (m_left->rightmost (), m_right->leftmost ())
   {}
 
-  leaf_op &
-  leftmost () override
+  std::pair <std::shared_ptr <exec_node>, std::shared_ptr <exec_node> >
+  build_exec (query::ptr q) override
   {
-    return m_left->leftmost ();
-  }
-
-  leaf_op &
-  rightmost () override
-  {
-    return m_right->rightmost ();
-  }
-};
-
-struct op_uni
-  : public leaf_op
-{
-  void
-  operate (query const &q) override
-  {
-    while (valfile::ptr vf = next ())
-      {
-	for (int i = 0; i < 25000; ++i)
-	  {
-	    auto vf2 = std::make_unique <valfile> (*vf);
-	    vf2->push_back (i);
-	    push (std::move (vf2));
-	  }
-      }
-  }
-
-  std::string
-  name () const override
-  {
-    return "uni";
+    auto le = m_left->build_exec (q);
+    auto re = m_right->build_exec (q);
+    auto qe = std::make_shared <que_node> (le.second, re.first);
+    le.second->assign_out (qe);
+    re.first->assign_in (qe);
+    return std::make_pair (le.first, re.second);
   }
 };
 
-struct op_dup
-  : public leaf_op
+class expr_uni
+  : public expr
 {
-  void
-  operate (query const &q) override
+  struct e
+    : public exec_node
   {
-    while (valfile::ptr vf = next ())
-      {
-	assert (! vf->empty ());
-	vf->push_back (vf->back ());
-	push (std::move (vf));
-      }
-  }
+    void
+    operate () override
+    {
+      while (valfile::ptr vf = next ())
+	{
+	  for (int i = 0; i < 25; ++i)
+	    {
+	      auto vf2 = std::make_unique <valfile> (*vf);
+	      vf2->push_back (i);
+	      push (std::move (vf2));
+	    }
+	}
+    }
 
-  std::string
-  name () const override
-  {
-    return "dup";
-  }
-};
-
-struct op_mul
-  : public leaf_op
-{
-  void
-  operate (query const &q) override
-  {
-    while (valfile::ptr vf = next ())
-      {
-	assert (vf->size () >= 2);
-	int a = vf->back (); vf->pop_back ();
-	int b = vf->back (); vf->pop_back ();
-	vf->push_back (a * b);
-	push (std::move (vf));
-      }
-  }
-
-  std::string
-  name () const override
-  {
-    return "mul";
-  }
-};
-
-class op_query
-  : public leaf_op
-{
-  std::unique_ptr <op> m_op;
-  que m_first;
-  que m_last;
-  ucontext m_main_ctx;
+    std::string
+    name () const override
+    {
+      return "uni";
+    }
+  };
 
 public:
-  explicit op_query (std::unique_ptr <op> op)
-    : m_op (std::move (op))
-    , m_first (*this, m_op->leftmost ())
-    , m_last (m_op->rightmost (), *this)
+  std::pair <std::shared_ptr <exec_node>, std::shared_ptr <exec_node> >
+  build_exec (query::ptr q) override
   {
-    if (getcontext (&m_main_ctx) == -1)
-      throw_system ();
-  }
-
-  void
-  operate (query const &q) override
-  {
-    push (std::make_unique <valfile> ());
-
-    while (valfile::ptr vf = next ())
-      {
-	std::cout << "[";
-	for (auto v: *vf)
-	  std::cout << " " << v;
-	std::cout << " ]\n";
-      }
-
-    // next () is written in such a way that if the input que is
-    // empty, but there's stuff in the output que, we switch right
-    // (downstream).  So we only get here if the first guy in pipeline
-    // got out of what he can produce, which signifies the end of
-    // query.
-    quit (m_main_ctx);
-  }
-
-  std::string
-  name () const override
-  {
-    return "query";
-  }
-
-  void
-  run ()
-  {
-    activate (m_main_ctx);
+    auto ret = std::make_shared <e> ();
+    return std::make_pair (ret, ret);
   }
 };
 
-int main(int argc, char *argv[])
+class expr_dup
+  : public expr
 {
-  auto U = std::make_unique <op_uni> ();
-  auto D = std::make_unique <op_dup> ();
-  auto M = std::make_unique <op_mul> ();
+  struct e
+    : public exec_node
+  {
+    void
+    operate () override
+    {
+      while (valfile::ptr vf = next ())
+	{
+	  assert (! vf->empty ());
+	  vf->push_back (vf->back ());
+	  push (std::move (vf));
+	}
+    }
 
-  auto C2 = std::make_unique <op_cat> (std::move (D), std::move (M));
-  auto C1 = std::make_unique <op_cat> (std::move (U), std::move (C2));
+    std::string
+    name () const override
+    {
+      return "dup";
+    }
+  };
 
-  auto Q = std::make_unique <op_query> (std::move (C1));
+public:
+  std::pair <std::shared_ptr <exec_node>, std::shared_ptr <exec_node> >
+  build_exec (query::ptr q) override
+  {
+    auto ret = std::make_shared <e> ();
+    return std::make_pair (ret, ret);
+  }
+};
 
-  Q->run ();
-  std::cout << "done\n";
-  //Q->operate (q);
+class expr_mul
+  : public expr
+{
+  struct e
+    : public exec_node
+  {
+    void
+    operate () override
+    {
+      while (valfile::ptr vf = next ())
+	{
+	  assert (vf->size () >= 2);
+	  int a = vf->back (); vf->pop_back ();
+	  int b = vf->back (); vf->pop_back ();
+	  vf->push_back (a * b);
+	  push (std::move (vf));
+	}
+    }
+
+    std::string
+    name () const override
+    {
+      return "mul";
+    }
+  };
+
+public:
+  std::pair <std::shared_ptr <exec_node>, std::shared_ptr <exec_node> >
+  build_exec (query::ptr q) override
+  {
+    auto ret = std::make_shared <e> ();
+    return std::make_pair (ret, ret);
+  }
+};
+
+class expr_query
+  : public expr
+{
+public:
+  class e
+    : public exec_node
+  {
+    ucontext m_main_ctx;
+
+  public:
+    e ()
+      : m_main_ctx ({})
+    {
+    }
+
+    void
+    operate () override
+    {
+      push (std::make_unique <valfile> ());
+
+      while (valfile::ptr vf = next ())
+	{
+	  std::cout << "[";
+	  for (auto v: *vf)
+	    std::cout << " " << v;
+	  std::cout << " ]\n";
+	}
+
+      // next () is written in such a way that if the input que is
+      // empty, but there's stuff in the output que, we switch right
+      // (downstream).  So we only get here if the first guy in pipeline
+      // got out of what he can produce, which signifies the end of
+      // query.
+      quit (m_main_ctx);
+    }
+
+    std::string
+    name () const override
+    {
+      return "query";
+    }
+
+    void
+    run ()
+    {
+      if (getcontext (&m_main_ctx) == -1)
+	throw_system ();
+      activate (m_main_ctx);
+      std::cout << "finished running\n";
+    }
+  };
+
+  std::unique_ptr <expr> m_expr;
+
+public:
+  explicit expr_query (std::unique_ptr <expr> expr)
+    : m_expr (std::move (expr))
+  {}
+
+  std::pair <std::shared_ptr <exec_node>, std::shared_ptr <exec_node> >
+  build_exec (query::ptr q) override
+  {
+    auto ee = m_expr->build_exec (q);
+    auto qr = std::make_shared <e> ();
+    auto lqi = std::make_shared <que_node> (qr, ee.first);
+    auto lqo = std::make_shared <que_node> (ee.second, qr);
+
+    ee.first->assign_in (lqi);
+    ee.second->assign_out (lqo);
+
+    qr->assign_in (lqo);
+    qr->assign_out (lqi);
+
+    return std::make_pair (qr, qr);
+  }
+};
+
+int
+main(int argc, char *argv[])
+{
+  auto U = std::make_unique <expr_uni> ();
+  auto D = std::make_unique <expr_dup> ();
+  auto M = std::make_unique <expr_mul> ();
+
+  auto C2 = std::make_unique <expr_cat> (std::move (D), std::move (M));
+  auto C1 = std::make_unique <expr_cat> (std::move (U), std::move (C2));
+
+  auto Q = std::make_unique <expr_query> (std::move (C1));
+
+  auto ctx = std::make_shared <query> ();
+  auto E = Q->build_exec (ctx);
+
+  if (auto q = dynamic_cast <expr_query::e *> (&*E.first))
+    q->run ();
+
+  std::cout << "over and out\n";
 
   return 0;
 }
