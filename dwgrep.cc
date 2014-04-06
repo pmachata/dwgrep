96861c0b107cfda05b410d40647bebbb8c5ea1df
Petr Machata <pmachata@redhat.com>
2014-04-06 20:43:03 +0200

    Support -e, -c, -H, -h, -q, -s like grep does

--- dwgrep.cc
+++ dwgrep.cc
@@ -2,6 +2,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <getopt.h>
 
 #include <cassert>
 #include <exception>
@@ -29,9 +30,9 @@ namespace
 }
 
 std::shared_ptr <Dwarf>
-open_dwarf (char const *fn)
+open_dwarf (std::string const &fn)
 {
-  int fd = open (fn, O_RDONLY);
+  int fd = open (fn.c_str (), O_RDONLY);
   if (fd == -1)
     throw_system ();
 
@@ -47,24 +48,152 @@ main(int argc, char *argv[])
 {
   elf_version (EV_CURRENT);
 
-  assert (argc == 3);
+  static option long_options[] = {
+    {"quiet", no_argument, nullptr, 'q'},
+    {"silent", no_argument, nullptr, 'q'},
+    {"no-messages", no_argument, nullptr, 's'},
+    {"expr", required_argument, nullptr, 'e'},
+    {"count", no_argument, nullptr, 'c'},
+    {"with-filename", no_argument, nullptr, 'H'},
+    {"no-filename", no_argument, nullptr, 'h'},
+    {nullptr, no_argument, nullptr, 0},
+  };
+  static char const *options = "ce:Hhqs";
 
-  tree t = parse_query (argv[2]);
-  size_t stk_depth = t.determine_stack_effects ();
-  t.dump (std::cout);
-  std::cout << std::endl;
+  bool quiet = false;
+  bool no_messages = false;
+  bool show_count = false;
+  bool with_filename = false;
+  bool no_filename = false;
 
-  auto q = std::make_shared <dwgrep_graph> (open_dwarf (argv[1]));
-  auto program = t.build_exec (nullptr, q, stk_depth);
+  std::vector <std::string> to_process;
 
-  while (valfile::uptr result = program->next ())
+  tree query;
+  bool seen_query = false;
+
+  while (true)
     {
-      auto value = result->get_slot (slot_idx (0));
-      value->show (std::cout);
-      std::cout << std::endl;
+      int c = getopt_long (argc, argv, options, long_options, nullptr);
+      if (c == -1)
+	break;
+
+      switch (c)
+	{
+	case 'e':
+	  query = parse_query (optarg);
+	  seen_query = true;
+	  break;
+
+	case 'c':
+	  show_count = true;
+	  break;
+
+	case 'H':
+	  with_filename = true;
+	  break;
+
+	case 'h':
+	  no_filename = true;
+	  break;
+
+	case 'q':
+	  quiet = true;
+	  break;
+
+	case 's':
+	  no_messages = true;
+	  break;
+
+	default:
+	  std::exit (2);
+	}
     }
 
-  std::cout << std::endl;
+  argc -= optind;
+  argv += optind;
 
-  return 0;
+  if (! seen_query)
+    {
+      query = parse_query (*argv++);
+      argc--;
+    }
+
+  size_t stk_depth = query.determine_stack_effects ();
+  if (! quiet)
+    {
+      query.dump (std::cerr);
+      std::cerr << std::endl;
+    }
+
+  if (argc == 0)
+    {
+      std::cerr << "No input files.\n";
+      return 2;
+    }
+  else
+    for (int i = 0; i < argc; ++i)
+      to_process.push_back (argv[i]);
+
+  if (to_process.size () > 1)
+    with_filename = true;
+  if (no_filename)
+    with_filename = false;
+
+  bool errors = false;
+  bool match = false;
+  for (auto const &fn: to_process)
+    {
+      std::shared_ptr <dwgrep_graph> g;
+
+      try
+	{
+	  g = std::make_shared <dwgrep_graph> (open_dwarf (fn));
+	}
+      catch (std::runtime_error e)
+	{
+	  if (! no_messages)
+	    std::cout << "dwgrep: " << fn << ": " << e.what () << std::endl;
+	  if (! quiet)
+	    errors = true;
+	  continue;
+	}
+
+      auto program = query.build_exec (nullptr, g, stk_depth);
+
+      uint64_t count = 0;
+      while (valfile::uptr result = program->next ())
+	{
+	  // grep: Exit immediately with zero status if any match
+	  // is found, even if an error was detected.
+	  if (quiet)
+	    std::exit (0);
+
+	  match = true;
+	  if (! show_count)
+	    {
+	      if (with_filename)
+		std::cout << fn << ":";
+	      auto value = result->get_slot (slot_idx (0));
+	      value->show (std::cout);
+	      std::cout << "\n";
+	    }
+	  else
+	    ++count;
+	}
+
+      if (show_count)
+	{
+	  if (with_filename)
+	    std::cout << fn << ":";
+	  std::cout << std::dec << count << std::endl;
+	}
+    }
+
+  if (errors)
+    std::exit (2);
+
+  if (match)
+    std::exit (0);
+  else
+    std::exit (1);
 }
