b97b87f31a777c727e36024c6c0e735b37781983
Petr Machata <pmachata@redhat.com>
2014-12-04 21:48:35 +0100

    Run the tests that don't need Dwarf on empty stack

--- tests/tests.sh
+++ tests/tests.sh
@@ -20,71 +20,71 @@ expect_count ()
     fi
 }
 
-expect_count 1 ./empty -e '1   10 ?lt'
-expect_count 1 ./empty -e '10  10 !lt'
-expect_count 1 ./empty -e '100 10 !lt'
-expect_count 1 ./empty -e '1   10 ?le'
-expect_count 1 ./empty -e '10  10 ?le'
-expect_count 1 ./empty -e '100 10 !le'
-expect_count 1 ./empty -e '1   10 !eq'
-expect_count 1 ./empty -e '10  10 ?eq'
-expect_count 1 ./empty -e '100 10 !eq'
-expect_count 1 ./empty -e '1   10 ?ne'
-expect_count 1 ./empty -e '10  10 !ne'
-expect_count 1 ./empty -e '100 10 ?ne'
-expect_count 1 ./empty -e '1   10 !ge'
-expect_count 1 ./empty -e '10  10 ?ge'
-expect_count 1 ./empty -e '100 10 ?ge'
-expect_count 1 ./empty -e '1   10 !gt'
-expect_count 1 ./empty -e '10  10 !gt'
-expect_count 1 ./empty -e '100 10 ?gt'
+expect_count 1 -e '1   10 ?lt'
+expect_count 1 -e '10  10 !lt'
+expect_count 1 -e '100 10 !lt'
+expect_count 1 -e '1   10 ?le'
+expect_count 1 -e '10  10 ?le'
+expect_count 1 -e '100 10 !le'
+expect_count 1 -e '1   10 !eq'
+expect_count 1 -e '10  10 ?eq'
+expect_count 1 -e '100 10 !eq'
+expect_count 1 -e '1   10 ?ne'
+expect_count 1 -e '10  10 !ne'
+expect_count 1 -e '100 10 ?ne'
+expect_count 1 -e '1   10 !ge'
+expect_count 1 -e '10  10 ?ge'
+expect_count 1 -e '100 10 ?ge'
+expect_count 1 -e '1   10 !gt'
+expect_count 1 -e '10  10 !gt'
+expect_count 1 -e '100 10 ?gt'
 
-expect_count 0 ./empty -e '1   10 !lt'
-expect_count 0 ./empty -e '10  10 ?lt'
-expect_count 0 ./empty -e '100 10 ?lt'
-expect_count 0 ./empty -e '1   10 !le'
-expect_count 0 ./empty -e '10  10 !le'
-expect_count 0 ./empty -e '100 10 ?le'
-expect_count 0 ./empty -e '1   10 ?eq'
-expect_count 0 ./empty -e '10  10 !eq'
-expect_count 0 ./empty -e '100 10 ?eq'
-expect_count 0 ./empty -e '1   10 !ne'
-expect_count 0 ./empty -e '10  10 ?ne'
-expect_count 0 ./empty -e '100 10 !ne'
-expect_count 0 ./empty -e '1   10 ?ge'
-expect_count 0 ./empty -e '10  10 !ge'
-expect_count 0 ./empty -e '100 10 !ge'
-expect_count 0 ./empty -e '1   10 ?gt'
-expect_count 0 ./empty -e '10  10 ?gt'
-expect_count 0 ./empty -e '100 10 !gt'
+expect_count 0 -e '1   10 !lt'
+expect_count 0 -e '10  10 ?lt'
+expect_count 0 -e '100 10 ?lt'
+expect_count 0 -e '1   10 !le'
+expect_count 0 -e '10  10 !le'
+expect_count 0 -e '100 10 ?le'
+expect_count 0 -e '1   10 ?eq'
+expect_count 0 -e '10  10 !eq'
+expect_count 0 -e '100 10 ?eq'
+expect_count 0 -e '1   10 !ne'
+expect_count 0 -e '10  10 ?ne'
+expect_count 0 -e '100 10 !ne'
+expect_count 0 -e '1   10 ?ge'
+expect_count 0 -e '10  10 !ge'
+expect_count 0 -e '100 10 !ge'
+expect_count 0 -e '1   10 ?gt'
+expect_count 0 -e '10  10 ?gt'
+expect_count 0 -e '100 10 !gt'
 
 
-expect_count 1 ./empty -e '1   != 10'
-expect_count 1 ./empty -e '10  == 10'
-expect_count 1 ./empty -e '100 != 10'
-expect_count 1 ./empty -e '1   <  10'
-expect_count 1 ./empty -e '10  <= 10'
-expect_count 1 ./empty -e '10  >= 10'
-expect_count 1 ./empty -e '100 >  10'
+expect_count 1 -e '1   != 10'
+expect_count 1 -e '10  == 10'
+expect_count 1 -e '100 != 10'
+expect_count 1 -e '1   <  10'
+expect_count 1 -e '10  <= 10'
+expect_count 1 -e '10  >= 10'
+expect_count 1 -e '100 >  10'
 
-expect_count 0 ./empty -e '1   == 10'
-expect_count 0 ./empty -e '10  != 10'
-expect_count 0 ./empty -e '100 == 10'
-expect_count 0 ./empty -e '1   >  10'
-expect_count 0 ./empty -e '10  < 10'
-expect_count 0 ./empty -e '10  > 10'
-expect_count 0 ./empty -e '100 < 10'
+expect_count 0 -e '1   == 10'
+expect_count 0 -e '10  != 10'
+expect_count 0 -e '100 == 10'
+expect_count 0 -e '1   >  10'
+expect_count 0 -e '10  < 10'
+expect_count 0 -e '10  > 10'
+expect_count 0 -e '100 < 10'
 
 # Test comments.
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	1 #blah blah -45720352304573257230453045302 {}[*[{)+*]&+*
 	2 # sub mul div mod
 	add == 3'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	1 //blah blah -45720352304573257230453045302 {}[*[{)+*]&+*
 	2 // sub mul div mod
 	add == 3'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	1 /*blah blah -45720352304573257230453045302 {}[*[{)+*]&+***/ 2 /*
 	sub mul div mod*/ add
 	== 3'
@@ -110,11 +110,11 @@ expect_count 11 ./nontrivial-types.o -e '
        	let A := entry; let B := A unit entry; (A pos == B pos)'
 
 # Test a bug with scope promotion.
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	let A:=1; ?(let X:=A;)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	0 [|B| 1 [|V| B]] ?([[0]] ?eq)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	1 {|A| 3 [|B| A]} apply ?([1] ?eq)'
 
 # Test that elem annotates position.
@@ -123,19 +123,19 @@ expect_count 1 ./nontrivial-types.o -e '
 	?(elem (pos == 0) (== 10))
 	?(elem (pos == 1) (== 11))
 	?(elem (pos == 2) (== 12))'
-expect_count 3 ./empty -e '
+expect_count 3 -e '
 	[0, 1, 2] elem dup (== pos)'
-expect_count 3 ./empty -e '
+expect_count 3 -e '
 	[2, 1, 0] relem dup (== pos)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	["210" relem] == ["012" elem]'
 
 # Check literal assertions.
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	?([1, 3, 5] elem (pos == 0) (== 1))
 	?([1, 3, 5] elem (pos == 1) (== 3))
 	?([1, 3, 5] elem (pos == 2) (== 5))'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	?([[1, 3, 5] elem (pos != 0) ((3,5) ==)] (length == 2))
 	?([[1, 3, 5] elem (pos != 1) ((1,5) ==)] (length == 2))
 	?([[1, 3, 5] elem (pos != 2) ((1,3) ==)] (length == 2))'
@@ -220,51 +220,51 @@ expect_count 1 ./enum.o -e '
 	entry (@AT_name == "e") child label "%d" "40" ?eq'
 
 # Check decoding of huge literals.
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	[0xffffffffffffffff "%s" elem !(pos == (0,1))]
 	(length == 16) !(elem != "f")'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	18446744073709551615 == 0xffffffffffffffff'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	01777777777777777777777 == 0xffffffffffffffff'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	0b1111111111111111111111111111111111111111111111111111111111111111
 	== 0xffffffffffffffff'
-expect_count 1 ./empty -e'
+expect_count 1 -e'
 	-0xff dup dup dup "%s %d %b %o" == "-0xff -255 -0b11111111 -0377"'
-expect_count 1 ./empty -e'
+expect_count 1 -e'
 	-0377 dup dup dup "%x %d %b %s" == "-0xff -255 -0b11111111 -0377"'
-expect_count 1 ./empty -e'
+expect_count 1 -e'
 	-255 dup dup dup "%x %s %b %o" == "-0xff -255 -0b11111111 -0377"'
-expect_count 1 ./empty -e'
+expect_count 1 -e'
 	-0b11111111 dup dup dup "%x %d %s %o"
 	== "-0xff -255 -0b11111111 -0377"'
 
 # Check arithmetic.
-expect_count 1 ./empty -e '-1 1 add (== 0)'
-expect_count 1 ./empty -e '-1 10 add (== 9)'
-expect_count 1 ./empty -e '1 -10 add (== -9)'
-expect_count 1 ./empty -e '-10 1 add (== -9)'
-expect_count 1 ./empty -e '10 -1 add (== 9)'
+expect_count 1 -e '-1 1 add (== 0)'
+expect_count 1 -e '-1 10 add (== 9)'
+expect_count 1 -e '1 -10 add (== -9)'
+expect_count 1 -e '-10 1 add (== -9)'
+expect_count 1 -e '10 -1 add (== 9)'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	-1 0xffffffffffffffff add "%s" == "0xfffffffffffffffe"'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	0xffffffffffffffff -1 add "%s" == "0xfffffffffffffffe"'
 
-expect_count 1 ./empty -e '-1 1 sub (== -2)'
-expect_count 1 ./empty -e '-1 10 sub (== -11)'
-expect_count 1 ./empty -e '1 -10 sub (== 11)'
-expect_count 1 ./empty -e '-10 1 sub (== -11)'
-expect_count 1 ./empty -e '10 -1 sub (== 11)'
+expect_count 1 -e '-1 1 sub (== -2)'
+expect_count 1 -e '-1 10 sub (== -11)'
+expect_count 1 -e '1 -10 sub (== 11)'
+expect_count 1 -e '-10 1 sub (== -11)'
+expect_count 1 -e '10 -1 sub (== 11)'
 
-expect_count 1 ./empty -e '-2 2 mul (== -4)'
-expect_count 1 ./empty -e '-2 10 mul (== -20)'
-expect_count 1 ./empty -e '2 -10 mul (== -20)'
-expect_count 1 ./empty -e '-10 2 mul (== -20)'
-expect_count 1 ./empty -e '10 -2 mul (== -20)'
-expect_count 1 ./empty -e '-10 -2 mul (== 20)'
-expect_count 1 ./empty -e '-2 -10 mul (== 20)'
+expect_count 1 -e '-2 2 mul (== -4)'
+expect_count 1 -e '-2 10 mul (== -20)'
+expect_count 1 -e '2 -10 mul (== -20)'
+expect_count 1 -e '-10 2 mul (== -20)'
+expect_count 1 -e '10 -2 mul (== -20)'
+expect_count 1 -e '-10 -2 mul (== 20)'
+expect_count 1 -e '-2 -10 mul (== 20)'
 
 # Check iterating over empty compile unit.
 expect_count 1 ./empty -e '
@@ -279,23 +279,23 @@ expect_count 6 ./typedef.o -e '
 	entry (@AT_decl_line || @AT_byte_size || drop 42)'
 expect_count 1 ./typedef.o -e '
 	entry (@AT_decl_line || @AT_byte_size || drop 42) (== 42)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	(0, 1, 20) (== 10 || == 20)'
 
 # Check closures.
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	{|A| {|B| A}} 2 swap apply 9 swap apply (== 1 1 add)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	{|A| {A}} 5 swap apply apply (== 2 3 add)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	let double := {dup add}; (1 double == 2)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	let adder := {|x| {|y| x y add}};
 	3 adder 2 swap apply (== 5)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	let map := {|f| [|L| L elem f]};
 	[1, 2, 3] {1 add} map ?([2, 3, 4] ?eq)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	let slice := {|L B E|
 	  let wrap := {|V|
 	    let X := if (V 0 ?ge) then (V) else (L length V add);
@@ -310,20 +310,20 @@ expect_count 1 ./empty -e '
 	?(-2 -1 slice [8] ?eq)'
 
 # Check that bindings remember position.
-expect_count 3 ./empty -e '
+expect_count 3 -e '
 	let E := [0, 1, 2] elem; E (== pos)'
 
 # Check recursion.
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	{|A| (?(A 10 ?ge) 0 || A 1 add F 1 add)} ->F;
 	0 F
 	?(10 ?eq)'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	{|F T| ?(F T ?le) F, ?(F T ?lt) F 1 add T seq} -> seq;
 	[1 10 seq] ?([1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ?eq)'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	{|N| (?(N 2 ?lt) 1 || N 1 sub fact N mul)} -> fact;
 	?(5 fact 120 ?eq)
 	?(6 fact 720 ?eq)
@@ -349,9 +349,9 @@ expect_count 1 ./nontrivial-types.o -e '
 	  (?TAG_structure_type, ?TAG_class_type))'
 
 # Check ifelse.
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	[if ?(1) then (2,3) else (4,5)] ?([2,3] ?eq)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	[if !(1) then (2,3) else (4,5)] ?([4,5] ?eq)'
 expect_count 6 ./typedef.o -e '
 	[entry] ?(length 6 ?eq)
@@ -359,7 +359,7 @@ expect_count 6 ./typedef.o -e '
 	?(("1 0xb","0 0x1d","0 0x28","0 0x2f","0 0x3a","0 0x45") ?eq)'
 
 # Check various Dwarf operators.
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	[[DW_AT_name, DW_TAG_const_type, DW_FORM_ref_sig8, DW_LANG_Go,
 	  DW_INL_inlined, DW_ATE_UTF, DW_ACCESS_private, DW_VIS_exported,
 	  DW_ID_case_insensitive, DW_VIRTUALITY_virtual, DW_CC_nocall,
@@ -369,7 +369,7 @@ expect_count 1 ./empty -e '
 	[0x3, 0x26, 0x20, 0x16, 0x1, 0x10, 0x3, 0x2, 0x3, 0x1, 0x3, 0x1, 0x1,
 	 0x28, 0x5, 0x0, 0x2, 0x3, 0x6] ?eq'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	let Dw := "duplicate-const" dwopen;
 
 	"%([Dw entry label]%)"
@@ -411,7 +411,7 @@ expect_count 1 ./empty -e '
 	!(!DW_FORM_strp || form !DW_FORM_strp)'
 
 # check type constants
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	?(1 type T_CONST ?eq "%s" "T_CONST" ?eq)
 	?("" type T_STR ?eq "%s" "T_STR" ?eq)
 	?([] type T_SEQ ?eq "%s" "T_SEQ" ?eq)
@@ -421,15 +421,15 @@ expect_count 1 ./duplicate-const -e '
 	?(entry ?root attribute ?AT_name type T_ATTR ?eq "%s" "T_ATTR" ?eq)'
 
 # Check some list and seq words.
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	[1] !empty !(?empty) ?(length 1 ?eq)
 	[] ?empty !(!empty) ?(length 0 ?eq)
 	"1" !empty !(?empty) ?(length 1 ?eq)
 	"" ?empty !(!empty) ?(length 0 ?eq)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	?([1, 2, 3] [4, 5, 6] add [1, 2, 3, 4, 5, 6] ?eq)
 	?("123" "456" add "123456" ?eq)'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	?("123456" ?("234" ?find) ?("123" ?find) ?("456" ?find) ?(dup ?find)
 		   !("234" !find) !("123" !find) !("456" !find) !(dup !find)
 		   ?("1234567" !find) !("1234567" ?find))
@@ -438,7 +438,7 @@ expect_count 1 ./empty -e '
 			!([2,3,4] !find) !([1,2,3] !find)
 			!([4,5,6] !find) !(dup !find)
 			?([1,2,3,4,5,6,7] !find) !([1,2,3,4,5,6,7] ?find))'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	?("123456" ?("234" !starts) ?("123" ?starts) ?("456" !starts) ?(dup ?starts)
 		   !("234" ?starts) !("123" !starts) !("456" ?starts) !(dup !starts)
 		   ?("1234567" !starts) !("1234567" ?starts))
@@ -447,7 +447,7 @@ expect_count 1 ./empty -e '
 			!([2,3,4] ?starts) !([1,2,3] !starts)
 			!([4,5,6] ?starts) !(dup !starts)
 			?([1,2,3,4,5,6,7] !starts) !([1,2,3,4,5,6,7] ?starts))'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	?("123456" ?("234" !ends) ?("123" !ends) ?("456" ?ends) ?(dup ?ends)
 		   !("234" ?ends) !("123" ?ends) !("456" !ends) !(dup !ends)
 		   ?("1234567" !ends) !("1234567" ?ends))
@@ -501,13 +501,13 @@ expect_count 2 ./duplicate-const -e '
 expect_count 2 ./duplicate-const -e '
 	entry (low == 0x4004cd) (high == 0x4004fb)'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	10 20 aset
 	?(9 !contains) ?(10 ?contains) ?(11 ?contains) ?(12 ?contains)
 	?(13 ?contains) ?(14 ?contains) ?(15 ?contains) ?(16 ?contains)
 	?(17 ?contains) ?(18 ?contains) ?(19 ?contains) ?(20 !contains)'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	10 20 aset
 	?(9 20 aset !contains) ?(10 20 aset ?contains)
 	?(11 20 aset ?contains) ?(12 20 aset ?contains)
@@ -516,7 +516,7 @@ expect_count 1 ./empty -e '
 	?(17 20 aset ?contains) ?(18 20 aset ?contains)
 	?(19 20 aset ?contains)'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	10 20 aset
 	?(9 10 aset !contains)
 	?(9 11 aset !contains) ?(9 12 aset !contains)
@@ -526,7 +526,7 @@ expect_count 1 ./empty -e '
 	?(9 19 aset !contains) ?(9 20 aset !contains)
 	?(9 21 aset !contains)'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	10 20 aset
 	?(9 21 aset !contains) ?(10 21 aset !contains)
 	?(11 21 aset !contains) ?(12 21 aset !contains)
@@ -535,11 +535,11 @@ expect_count 1 ./empty -e '
 	?(17 21 aset !contains) ?(18 21 aset !contains)
 	?(19 21 aset !contains) ?(20 21 aset !contains)'
 
-expect_count 1 ./empty -e '10 20 aset 10 10 aset ?contains'
-expect_count 1 ./empty -e '10 20 aset 15 15 aset ?contains'
-expect_count 1 ./empty -e '10 10 aset dup ?contains'
+expect_count 1 -e '10 20 aset 10 10 aset ?contains'
+expect_count 1 -e '10 20 aset 15 15 aset ?contains'
+expect_count 1 -e '10 10 aset dup ?contains'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	5 10 aset
 	?(4 5 aset !overlaps) ?(4 6 aset ?overlaps)
 	?(4 7 aset ?overlaps) ?(4 8 aset ?overlaps)
@@ -549,18 +549,18 @@ expect_count 1 ./empty -e '
 	?(8 11 aset ?overlaps) ?(9 11 aset ?overlaps)
 	?(10 11 aset !overlaps)'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	5 10 aset !empty
 	5 5 aset ?empty'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	1 10 aset 2 20 aset overlap == 2 10 aset'
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	0x10 0x20 aset add: (0x30 0x40 aset) add: (0x50 0x60 aset)
 	overlap: (0x15 0x55 aset)
 	== 0x15 0x20 aset add: (0x30 0x40 aset) add: (0x50 0x55 aset)'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	(10 20 aset length == 10)
 	(10 10 aset length == 0)'
 
@@ -604,7 +604,7 @@ expect_count 4 ./float_const_value.o -e '
 	      (@AT_const_value == [0, 0, 0, 0, 0, 0, 0, 0,
 				   0, 0, 0, 0, 0, 0x80, 0xff, 0x3f]))))'
 
-expect_count 1 ./empty -e '
+expect_count 1 -e '
 	[1, 2, 3, 4] [3, 4, 5, 6]
 	(|A B| A [B elem !(== A elem)] add) == [1, 2, 3, 4, 5, 6]'
 
@@ -731,7 +731,7 @@ expect_count 1 ./inconsistent-types -e '
 # Test that zero bytes don't terminate the query too soon.
 TMP=$(mktemp)
 echo -e '7 == "foo\x00bar" length' > $TMP
-expect_count 1 ./empty -f $TMP
+expect_count 1 -f $TMP
 rm $TMP
 
 echo "$total tests total, $failures failures."
