6c3c7091a100539e33d3e28a803a39b2a7ad0d36
Petr Machata <pmachata@redhat.com>
2015-01-26 18:49:06 +0100

    Extract code related to importing GDB's dwarf.exp to a local module

--- tests/dwz-partial4.tcl
+++ tests/dwz-partial4.tcl
@@ -1,12 +1,9 @@
 # This test case uses dwarf.exp from GDB.
+# GDB_SRCPATH=/path/to/gdb/source tclsh dwz-partial4.tcl
+# gcc -c dwz-partial4-1.s
+# gcc -c dwz-partial4-C.s -o dwz-partial4-C
 
-set hex "0\[xX\]\[0-9a-fA-F\]+"
-set decimal "\[0-9\]+"
-set path $::env(GDB_SRCPATH)
-set tspath [file join $path gdb testsuite]
-set srcdir $tspath
-
-source [file join $tspath lib dwarf.exp]
+source "dwarf.exp"
 
 Dwarf::assemble "dwz-partial4-C.s" {
     build_id 0102030405060708
@@ -31,7 +28,3 @@ Dwarf::assemble "dwz-partial4-1.s" {
 	}
     }
 }
-
-# GDB_SRCPATH=/path/to/gdb/source tclsh dwz-partial4.tcl
-# gcc -c dwz-partial4-1.s
-# gcc -c dwz-partial4-C.s -o dwz-partial4-C
