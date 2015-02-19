59d07a7358b3055d3f297dfd4086f5a5206d82fa
Petr Machata <pmachata@redhat.com>
2015-02-19 01:56:53 +0100

    Fix version script hack to rm -f to handle first-time runs

--- libzwerg/build-version-script.sh
+++ libzwerg/build-version-script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ libzwerg.map -ot $1/libzwerg.map ]; then
-    cp $1/libzwerg.map libzwerg.map && rm libzwerg.so
+    cp $1/libzwerg.map libzwerg.map && rm -f libzwerg.so
 else
     true
 fi
