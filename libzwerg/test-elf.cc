e063262f399515ab9a38b4897498852b28b06744
Petr Machata <pmachata@gmail.com>
2018-02-04 16:47:06 +0100

    libzwerg: Add tests for 'elf' keyword
    Test that for separate-debuginfo files, 'elf' yields twice, and the
    names match.
    
    Signed-off-by: Petr Machata <pmachata@gmail.com>

---
+++ libzwerg/test-elf.cc
@@ -0,0 +1,52 @@
+/*
+   Copyright (C) 2018 Petr Machata
+   This file is part of dwgrep.
+
+   This file is free software; you can redistribute it and/or modify
+   it under the terms of either
+
+     * the GNU Lesser General Public License as published by the Free
+       Software Foundation; either version 3 of the License, or (at
+       your option) any later version
+
+   or
+
+     * the GNU General Public License as published by the Free
+       Software Foundation; either version 2 of the License, or (at
+       your option) any later version
+
+   or both in parallel, as here.
+
+   dwgrep is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received copies of the GNU General Public License and
+   the GNU Lesser General Public License along with this program.  If
+   not, see <http://www.gnu.org/licenses/>.  */
+
+#include <gtest/gtest.h>
+
+#include "test-zw-aux.hh"
+#include "test-dw-aux.hh"
+
+using namespace test;
+
+TEST_F (DwTest, test_elf)
+{
+  typedef std::vector <std::tuple <size_t, std::string, std::string>> vec;
+  for (auto const &entry: vec {
+	    {2, "twocus-copy", "elf"},
+	    {1, "twocus-copy", "elf \"%s\" =~ \"twocus-copy[^.]\""},
+	    {1, "twocus-copy", "elf \"%s\" =~ \"twocus-copy.dbg\""},
+	})
+    {
+      size_t nresults = std::get <0> (entry);
+      auto const &fn = std::get <1> (entry);
+      auto const &q = std::get <2> (entry);
+
+      auto yielded = run_dwquery (*builtins, fn, q);
+      ASSERT_EQ (nresults, yielded.size ());
+    }
+}
