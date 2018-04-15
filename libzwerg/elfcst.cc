b9d81de0e66f8181af9f62161777f80747631388
Petr Machata <pmachata@gmail.com>
2018-04-15 02:10:21 +0200

    libzwerg/elfcst: Extract linear map code into class
    Wrap in a class the code that handles separating associative array into
    linear and associative part. Make the EM_ handling code a subclass of
    that, use in elf_em_string.

--- libzwerg/elfcst.cc
+++ libzwerg/elfcst.cc
@@ -54,76 +54,88 @@ elf_et_string (int et, brevity brv)
 }
 
 
+class linear_map
+{
+  std::vector <const char *> m_linear;
+  std::map <unsigned, const char *> m_assoc;
+
+public:
+  linear_map (std::map <unsigned, const char *> all)
+  {
+    // Find the longest sequence that's advantageous to store linearly. On g++,
+    // map node has an overhead of four words, and unordered_map seems to fare
+    // in the same ballpark, plus the space for the key gives five words. So one
+    // useful entry for five wasted is still a fair deal, memory-wise (and very
+    // likely runtime-wise as well, since we do base add and dereference vs.
+    // pointer chasing in a map).
+
+    unsigned last = 0;   // The last item that's advantageous to keep in linear.
+    unsigned useful = 0; // Usefully-spent items in linear.
+    for (auto it = all.begin (); it != all.end (); ++it)
+      if ((it->first + 1) / 6 <= useful++)
+	last = it->first;
+
+    for (auto it = all.begin ();
+	 it->first <= last && it != all.end ();
+	 it = all.erase (it))
+      {
+	m_linear.resize (it->first, nullptr);
+	m_linear.push_back (it->second);
+      }
+
+    m_assoc = std::move (all);
+  }
+
+  char const *find (int key)
+  {
+    if (key < 0)
+      return nullptr;
+    return find (static_cast <unsigned> (key));
+  }
+
+  char const *find (unsigned key)
+  {
+    if (key < m_linear.size ())
+      return m_linear[key];
+
+    auto it = m_assoc.find (key);
+    if (it != m_assoc.end ())
+      return it->second;
+
+    return nullptr;
+  }
+};
+
+
 // Since there are aliases in ELF_ALL_KNOWN_EM (and we do want to keep them,
 // because it's still desirable to be able to use that name in a program), we
 // can't use the trick with switch. At least EM_ALPHA also has a massive value,
 // which precludes using a simple array storage for all names.
-static void
-elf_em_string_initialize (std::vector <char const *> &linear,
-			  std::map <unsigned, char const *> &assoc)
+struct elf_em_strings
+  : public linear_map
 {
-  std::map <unsigned, char const *> all;
-#define ELF_ONE_KNOWN_EM(NAME, CODE) all[CODE] = #CODE;
-  ELF_ALL_KNOWN_EM;
+  std::map <unsigned, char const *>
+  all () const
+  {
+    std::map <unsigned, char const *> ret;
+#define ELF_ONE_KNOWN_EM(NAME, CODE) ret[CODE] = #CODE;
+    ELF_ALL_KNOWN_EM
 #undef ELF_ONE_KNOWN_EM
+    return ret;
+  }
 
-  // Find the longest sequence that's advantageous to store linearly. On g++,
-  // map node has an overhead of four words, and unordered_map seems to fare in
-  // the same ballpark, plus the space for the key gives five words. So one
-  // useful entry for five wasted is still a fair deal, memory-wise (and very
-  // likely runtime-wise as well, since we do base add and dereference vs.
-  // pointer chasing in a map).
-
-  unsigned last = 0;   // The last item that's advantageous to keep in linear.
-  unsigned useful = 0; // Usefully-spent items in linear.
-  for (auto it = all.begin (); it != all.end (); ++it)
-    if ((it->first + 1) / 6 <= useful++)
-      last = it->first;
-
-  for (auto it = all.begin ();
-       it->first <= last && it != all.end ();
-       it = all.erase (it))
-    {
-      linear.resize (it->first, nullptr);
-      linear.push_back (it->second);
-    }
-
-  assoc = std::move (all);
-}
-
-static const char *
-__elf_em_string (unsigned em, brevity brv)
-{
-  static std::vector <const char *> linear;
-  static std::map <unsigned, const char *> assoc;
-  static bool initialized;
-  if (! initialized)
-    {
-      elf_em_string_initialize (linear, assoc);
-      initialized = true;
-    }
-
-  const char *ret = nullptr;
-  if (em < linear.size ())
-    ret = abbreviate (linear[em], sizeof "EM", brv);
-  else
-    {
-      auto it = assoc.find (em);
-      if (it != assoc.end ())
-	ret = abbreviate (it->second, sizeof "EM", brv);
-    }
-
-  return ret;
-}
+  elf_em_strings ()
+    : linear_map {all ()}
+  {}
+};
 
 static const char *
 elf_em_string (int em, brevity brv)
 {
-  if (em < 0)
-    return nullptr;
-  else
-    return __elf_em_string (static_cast <unsigned> (em), brv);
+  static elf_em_strings ss;
+  return abbreviate (ss.find (em), sizeof "EM", brv);
 }
+
 
 zw_cdom const &
 elf_class_dom ()
